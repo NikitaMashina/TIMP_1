35f9a72828e9f4197f1c7ec15b8ea884db3ea6a6
NikitaMashina <113506305+NikitaMashina@users.noreply.github.com>
2022-09-18 23:26:56 +0300

    Update modAlphaCipher.cpp

--- lab_3.1/modAlphaCipher.cpp
+++ lab_3.1/modAlphaCipher.cpp
@@ -1,20 +1,25 @@
 #include "modAlphaCipher.h"
-modAlphaCipher::modAlphaCipher(const wstring& skey)
+modAlphaCipher::modAlphaCipher(const string& skey)
 {
-    for (unsigned i=0; i<numAlpha.size(); i++) {
-        alphaNum[numAlpha[i]]=i;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(numAlpha); // перекодируем
+    for (unsigned i=0; i<ws.size(); i++) {
+        alphaNum[ws[i]]=i;
     }
     key = convert(skey);
 }
-wstring modAlphaCipher::encrypt(const wstring& open_text)
+
+string modAlphaCipher::encrypt(const string& open_text)
 {
-    vector<int> work = convert(open_text);
+   vector<int> work = convert(open_text);
     for(unsigned i=0; i < work.size(); i++) {
         work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
     }
     return convert(work);
 }
-wstring modAlphaCipher::decrypt(const wstring& cipher_text)
+
+string modAlphaCipher::decrypt(const string& cipher_text)
 {
     vector<int> work = convert(cipher_text);
     for(unsigned i=0; i < work.size(); i++) {
@@ -22,19 +27,41 @@ wstring modAlphaCipher::decrypt(const wstring& cipher_text)
     }
     return convert(work);
 }
-inline vector<int> modAlphaCipher::convert(const wstring& s)
+
+inline vector<int> modAlphaCipher::convert(const string& s)
 {
-    vector<int> result;
-    for(auto c:s) {
-        result.push_back(alphaNum[c]);
+   vector<int> result;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(s); // перекодируем
+    for (unsigned i=0; i<ws.size(); i++) {
+        result.push_back(alphaNum[ws[i]]);
     }
     return result;
 }
-inline wstring modAlphaCipher::convert(const vector<int>& v)
+
+inline string modAlphaCipher::convert(const vector<int>& v)
 {
-    wstring result;
-    for(auto i:v) {
-        result.push_back(numAlpha[i]);
+    string result;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(numAlpha);
+    wstring result_s = codec.from_bytes("");
+    for (unsigned i=0; i<v.size(); i++) {
+        result_s.push_back(ws[v[i]]);
     }
+    result = codec.to_bytes(result_s);
+    return result;
+}
+
+    std::string result;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(numAlpha);
+    wstring result_s = codec.from_bytes("");
+    for (unsigned i=0; i<v.size(); i++) {
+        result_s.push_back(ws[v[i]]);
+    }
+    result = codec.to_bytes(result_s);
     return result;
 }
